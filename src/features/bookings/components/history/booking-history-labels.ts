95208f50c400ec0d59f4578f905473b5641c08b2
Franco Lianza <lianza.fl@gmail.com>
2025-12-26 15:45:11 -0300

    Labels

--- src/features/bookings/components/history/booking-history-labels.ts
+++ src/features/bookings/components/history/booking-history-labels.ts
@@ -9,12 +9,33 @@ export function getActionLabel(action: BookingHistoryAction): string {
     [BookingHistoryAction.CUSTOMER_STATUS_CHANGE]: 'Cliente: Cambio de estado',
     [BookingHistoryAction.PROVIDER_STATUS_CHANGE]:
       'Proveedor: Cambio de estado',
-    [BookingHistoryAction.FILE_CUSTOMER_REBILLING]: 'Cliente: Refacturación',
+    [BookingHistoryAction.AUTO_PAYMENT_PROCESSED]: 'Pago Automático Procesado',
+    [BookingHistoryAction.FILE_CUSTOMER_PROFORMA_TO_SEND]:
+      'Cliente: Proforma a enviar',
+    [BookingHistoryAction.FILE_CUSTOMER_PROFORMA_SENT]:
+      'Cliente: Proforma enviada',
+    [BookingHistoryAction.FILE_CUSTOMER_AMOUNT_CHARGED]:
+      'Cliente: Importe cobrado',
     [BookingHistoryAction.FILE_CUSTOMER_TO_ACCOUNT]: 'Cliente: A contabilizar',
     [BookingHistoryAction.FILE_CUSTOMER_INVOICED]: 'Cliente: Facturado',
+    [BookingHistoryAction.FILE_CUSTOMER_PAYMENT_PENDING]:
+      'Cliente: Pago pendiente',
+    [BookingHistoryAction.FILE_CUSTOMER_INTERCOMPANY]: 'Cliente: Intercompany',
+    [BookingHistoryAction.FILE_CUSTOMER_REBILLING]: 'Cliente: Refacturación',
+    [BookingHistoryAction.FILE_CUSTOMER_CANCELLED]: 'Cliente: Cancelado',
+    [BookingHistoryAction.FILE_CUSTOMER_END]: 'Cliente: Finalizado',
+    [BookingHistoryAction.FILE_PROVIDER_TO_VALIDATE]: 'Proveedor: A validar',
+    [BookingHistoryAction.FILE_PROVIDER_WHITELIST]: 'Proveedor: Whitelist',
+    [BookingHistoryAction.FILE_PROVIDER_RECONFIRMED]: 'Proveedor: Reconfirmado',
+    [BookingHistoryAction.FILE_PROVIDER_EXPIRED_VALIDATION]:
+      'Proveedor: Validación expirada',
+    [BookingHistoryAction.FILE_PROVIDER_ERROR]: 'Proveedor: Error',
+    [BookingHistoryAction.FILE_PROVIDER_PAID]: 'Proveedor: Pagado',
     [BookingHistoryAction.FILE_PROVIDER_REQUEST_REFUND]:
       'Proveedor: Solicitar reembolso',
-    [BookingHistoryAction.AUTO_PAYMENT_PROCESSED]: 'Pago Automático Procesado'
+    [BookingHistoryAction.FILE_PROVIDER_REBILLING]: 'Proveedor: Refacturación',
+    [BookingHistoryAction.FILE_PROVIDER_CANCELLED]: 'Proveedor: Cancelado',
+    [BookingHistoryAction.FILE_PROVIDER_END]: 'Proveedor: Finalizado'
   };
   return labels[action] || action;
 }
